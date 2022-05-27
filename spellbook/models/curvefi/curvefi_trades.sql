60da6613635dbdf1e0c6eb7564dac03323694f57
soispoke <thomasthiery@dune.com>
2022-05-27 16:45:14 +0000

    expose curvefi

--- spellbook/models/curvefi/curvefi_trades.sql
+++ spellbook/models/curvefi/curvefi_trades.sql
@@ -3,8 +3,11 @@
         )
 }}
 
-SELECT blockchain, tx_hash, block_time, amount_usd, amount, token_symbol, token_address FROM 
-(SELECT blockchain, tx_hash, block_time, amount_usd, amount, token_symbol, token_address FROM {{ ref('curvefi_ethereum_trades') }} ) 
+SELECT blockchain, project, version, block_time, trader_a, trader_b, 
+       token_a_amount_raw, token_b_amount_raw, token_a_address, token_b_address, tx_hash, trace_address, evt_index FROM 
+(SELECT blockchain, project, version, block_time, trader_a, trader_b, 
+       token_a_amount_raw, token_b_amount_raw, token_a_address, token_b_address, tx_hash, trace_address, evt_index
+ FROM {{ ref('curvefi_ethereum_trades') }} ) 
 {% if is_incremental() %}
 -- this filter will only be applied on an incremental run
 where block_time > (select max(block_time) from {{ this }})
